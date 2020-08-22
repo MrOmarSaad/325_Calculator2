6160b206a0ec2074169181af49c988fb2f1dcda4
omar <omarsaad124@yahoo.com>
2020-08-22 16:42:25 +0200

    1st commit

---
+++ Project1/Source.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int add(int x, int y) {
+	return x + y;
+}
+int sub(int x, int y) {
+	return x - y;
+}
+void main() {
+	int res1 = add(5, 8);
+	int res2 = sub(20, 10);
+	cout << res1 << endl;
+	cout << res2 << endl;
+	system("pause");
+}
